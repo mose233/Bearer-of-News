b3074927f819fc1df046abaa4251a5f3b5d68be4
mose233 <enockmose743@gmail.com>
2026-06-01 21:30:27 +0300

    Update Supabase anon key in supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,11 +1,21 @@
 import { createClient } from "@supabase/supabase-js";
 
+
+
 const supabaseUrl =
+
   import.meta.env.VITE_SUPABASE_URL ||
+
   "https://bjclqqynzsljskfeqfdj.supabase.co";
 
+
+
 const supabaseAnonKey =
+
   import.meta.env.VITE_SUPABASE_ANON_KEY ||
-  "PASTE_YOUR_SUPABASE_ANON_KEY_HERE";
+
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJqY2xxcXluenNsanNrZmVxZmRqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzExNDM0OTgsImV4cCI6MjA4NjcxOTQ5OH0.eB5iol81f6Yrw3kLJnDnihcCevhDHvpAJMPT-uuBINc"; 
+
+
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
