5068ae9e363b6641e93af7c5fefea28b3d250511
mose233 <enockmose743@gmail.com>
2026-06-11 09:24:45 +0300

    Add voiceStyles array to voiceLibrary.ts

---
+++ src/lib/creator/voiceLibrary.ts
@@ -0,0 +1,13 @@
+export const voiceStyles = [
+  "Kenyan Radio Host",
+  "Nigerian Narrator",
+  "News Presenter",
+  "Motivational Speaker",
+  "Luxury Commercial Voice",
+  "Business Presenter",
+  "Gospel Female Voice",
+  "Gospel Male Voice",
+  "Afrobeats Male Voice",
+  "Afrobeats Female Voice",
+  "Story Teller",
+];
