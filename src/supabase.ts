e6895f1ff5fd83451633d96e462b2dc937da9e52
mose233 <enockmose743@gmail.com>
2026-06-19 13:40:47 +0300

    Delete src/supabase.ts

--- src/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://bjclqqynzsljskfeqfdj.supabase.co';
-const supabaseKey = 'sb_publishable_lrdCN_8hOVwRFf3ncP5Tzg_aa0ATnnX';
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
