ed50f3bdb7d017ed7b1a7e9c54e52237e99a0d1f
mose233 <enockmose743@gmail.com>
2026-06-25 07:17:28 -0400

    Extend timeout for project reset after export
    Increased the timeout duration for resetting the current project after export operations from 1 second to 10 seconds.

--- src/pages/CreatorStudio.tsx
+++ src/pages/CreatorStudio.tsx
@@ -899,7 +899,7 @@ const resetCurrentProject = () => {
       } finally {
   setTimeout(() => {
     resetCurrentProject();
-  }, 1000);
+  }, 10000);
 }
   };
 
@@ -923,7 +923,7 @@ const resetCurrentProject = () => {
 
     setTimeout(() => {
       resetCurrentProject();
-    }, 1000);
+    }, 10000);
   }
 };
   
@@ -952,7 +952,7 @@ const resetCurrentProject = () => {
 
     setTimeout(() => {
       resetCurrentProject();
-    }, 1000);
+    }, 10000);
   }
 };
 
