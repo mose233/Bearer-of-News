0fe5f8418dd9fecd9433d79955c2f900e736c3d9
mose233 <enockmose743@gmail.com>
2026-06-27 16:58:25 +0300

    Implement shareFile function for native sharing

--- src/lib/creator/android/AndroidShareManager.ts
+++ src/lib/creator/android/AndroidShareManager.ts
@@ -1,1 +1,35 @@
+import { supportsNativeShare } from "../DeviceManager";
 
+export async function shareFile(
+  blob: Blob,
+  filename: string
+): Promise<boolean> {
+  if (!supportsNativeShare()) {
+    return false;
+  }
+
+  try {
+    const file = new File([blob], filename, {
+      type: blob.type,
+    });
+
+    // @ts-ignore
+    if (
+      navigator.canShare &&
+      // @ts-ignore
+      !navigator.canShare({ files: [file] })
+    ) {
+      return false;
+    }
+
+    // @ts-ignore
+    await navigator.share({
+      files: [file],
+      title: filename,
+    });
+
+    return true;
+  } catch {
+    return false;
+  }
+}
