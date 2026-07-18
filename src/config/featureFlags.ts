c33303a30a7d0f7b5617268301031e877e93ebb0
mose233 <enockmose743@gmail.com>
2026-07-18 14:58:47 +0300

    Add featureFlags configuration file

---
+++ src/config/featureFlags.ts
@@ -0,0 +1,11 @@
+export const FEATURE_FLAGS = {
+  AI_ENABLED: false,      // Master switch
+
+  PICTURE_AI: true,
+  VIDEO_AI: true,
+  CINEMATIC_AI: true,
+  MUSIC_AI: true,
+
+  MPESA: false,
+  FAL_AI: false,
+};
