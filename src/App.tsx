08c5ca3f253bfc76898e513d57bf48deea31fddd
mose233 <enockmose743@gmail.com>
2026-07-20 16:01:38 +0300

    Add LaunchingSoon component to creator studio route

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import LaunchingSoon from "./pages/LaunchingSoon";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -84,15 +85,11 @@ const App = () => {
                   }
                 />
 
-                <Route
+               <Route
   path="/creator-studio"
   element={
     <ProtectedRoute>
-      {navigator.userAgent.toLowerCase().includes("android") ? (
-        <AndroidCreatorStudio />
-      ) : (
-        <CreatorStudio />
-      )}
+      <LaunchingSoon />
     </ProtectedRoute>
   }
 />
