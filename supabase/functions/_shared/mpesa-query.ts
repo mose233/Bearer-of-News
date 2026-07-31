719f9551600a488a6a7c2cd431cfba6b3e766639
mose233 <enockmose743@gmail.com>
2026-07-31 12:50:54 +0300

    Add MPESA STK status query function

---
+++ supabase/functions/_shared/mpesa-query.ts
@@ -0,0 +1,43 @@
+import {
+  MPESA_BASE_URL,
+  MPESA_SHORTCODE,
+} from "./env.ts";
+
+import { getAccessToken } from "./mpesa.ts";
+import { generatePassword, generateTimestamp } from "./mpesa-utils.ts";
+
+export async function querySTKStatus(checkoutRequestID: string) {
+  const accessToken = await getAccessToken();
+
+  const timestamp = generateTimestamp();
+  const password = generatePassword(timestamp);
+
+  const response = await fetch(
+    `${MPESA_BASE_URL}/mpesa/stkpushquery/v1/query`,
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        BusinessShortCode: MPESA_SHORTCODE,
+        Password: password,
+        Timestamp: timestamp,
+        CheckoutRequestID: checkoutRequestID,
+      }),
+    }
+  );
+
+  const text = await response.text();
+
+  let data;
+
+  try {
+    data = JSON.parse(text);
+  } catch {
+    throw new Error("Invalid response from Daraja.");
+  }
+
+  return data;
+}
